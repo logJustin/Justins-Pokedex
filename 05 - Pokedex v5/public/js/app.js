d3ca33118e3fc89204270ecac3cef006eba7bf80
JustinShotMe <Reynolds.justinmichael@gmail.com>
2023-05-14 18:18:07 +0200

    init

--- 05 - Pokedex v5/public/js/app.js
+++
@@ -1,8 +0,0 @@
-
-// const searchBar = document.querySelector("#searchBar")
-// const searchButton = document.querySelector("#searchButton")
-
-// searchButton.addEventListener("click", (e) => {
-//     const inputURL = "http://localhost:3000/pokemon/" + searchBar.value
-//     window.location.href = inputURL
-// })
